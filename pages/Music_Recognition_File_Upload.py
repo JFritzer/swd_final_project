fdf0ce9ca4822f9af89befffe5c790e043eba0d4
Patrick Monthaler <mp4979@mci4me.at>
2024-02-17 12:59:04 +0100

    Database extended by two songs. Music import first attempt with class implementation. Added music recognition classes for database and full-length song verification.Database extended by two songs. Music import first attempt with class implementation. Added music recognition classes for database and full-length song verification.

--- pages/Music_Recognition_File_Upload.py
+++ pages/Music_Recognition_File_Upload.py
@@ -3,12 +3,11 @@ from tinydb import TinyDB, Query
 from typing import Optional, BinaryIO
 import os
 from music_analyzer import read_in
+from music_analyzer import recognise_song #(song_recognise, song_compare) -> int:
 import wave
 import settings
+from typing import Optional, Tuple
 
-
-# Verbindung zur Datenbank herstellen
-db = TinyDB('./db/multimedia_database.json')
 
 
 # Seiteneinstellungen festlegen
@@ -19,71 +18,107 @@ st.markdown("# Music recognition tool, using file upload")
 st.subheader("Here you can upload your file and the program gives you the name of the song back.")
 
 
-# Funktion zum Lesen des Audios und Berechnen der Hashes
-def calculate_hashes(audio_file: BinaryIO) -> Optional[str]:
-    """
-    Liest das hochgeladene Audiodatei ein und berechnet die Hashes.
+# Klasse für die Multimedia-Datenbank
+class MultimediaDatabase:
+    def __init__(self, db_path: str = './db/multimedia_database.json') -> None:
+        self.db = TinyDB(db_path)
 
-    Args:
-        audio_file (BinaryIO): Hochgeladene Audiodatei.
-
-    Returns:
-        str: Hashes des Audios.
-    """
-
-    audio = audio_file.read()
-    if audio_file is not None:
-        # Speichern der Audiodatei temporär auf dem Server
-        with wave.open("uploaded_audio.wav", "wb") as audio_f:
-            audio_f.setnchannels(settings.NUM_CHANNELS)
-            audio_f.setsampwidth(settings.BIT_DEPTH // 8)
-            audio_f.setframerate(settings.SAMPLE_RATE)
-            audio_f.setcomptype(settings.COMPRESSION_TYPE, 'NONE')
-            audio_f.writeframes(audio)
-        # Aufruf der read_in-Funktion mit dem Dateipfad
-        hashes = read_in("uploaded_audio.wav")
-        
-        # Löschen der temporären Audiodatei
-        os.remove("uploaded_audio.wav")
-        
-        return hashes
-    else:
-        return None
+    def get_entry_id_by_hashes(self, recognise_hashes: str) -> str:
+        """Hole die ID eines Eintrags aus der Datenbank anhand der Hashes."""
+        Entry = Query()
+        entry = self.db.get(Entry.hashes == recognise_hashes)
+        if entry:
+            return entry.get('id')
+        else:
+            return None
     
+    def find_matching_hashes(self, hashes: str) -> Optional[str]:
+        """
+        Sucht in der Datenbank nach übereinstimmenden Hashes.
 
-# Funktion zum Suchen nach passenden Hashes in der Datenbank
-def find_matching_hashes(hashes: str) -> Optional[str]:
-    """
-    Sucht in der Datenbank nach passenden Hashes und gibt die zugehörige ID zurück.
+        Args:
+            hashes (str): Die Hashes des hochgeladenen Audios.
 
-    Args:
-        hashes (str): Hashes des hochgeladenen Audios.
+        Returns:
+            Optional[str]: Die ID des gefundenen Eintrags oder None, wenn kein übereinstimmender Eintrag gefunden wurde.
+        """
+        matching_id = self.get_entry_id_by_hashes(hashes)
+        return matching_id
+    
+    def get_title_and_image_by_id(self, entry_id: str) -> Optional[Tuple[str, str]]:
+        """Holt den Titel und den Bildpfad eines Eintrags aus der Datenbank anhand der ID."""
+        Entry = Query()
+        entry = self.db.get(Entry.id == entry_id)
+        if entry:
+            title = entry.get('title')
+            image_path = entry.get('image_file_path')
+            return title, image_path
+        else:
+                return None, None
 
-    Returns:
-        Optional[str]: Die ID des passenden Eintrags in der Datenbank, oder None, wenn kein passender Eintrag gefunden wurde.
-    """
-    Entry = Query()
-    result = db.get(Entry.hashes == hashes)
-    if result:
-        return result.get('id')
-    else:
-        return None
+    
+class SongImport:
+    def __init__(self):
+        pass
 
+    # Funktion zum Lesen des Audios und Berechnen der Hashes
+    def calculate_hashes(self, audio_file: BinaryIO) -> Optional[str]:
+        """
+        Liest das hochgeladene Audiodatei ein und berechnet die Hashes.
+
+        Args:
+            audio_file (BinaryIO): Hochgeladene Audiodatei.
+
+        Returns:
+            str: Hashes des Audios.
+        """
+
+        audio = audio_file.read()
+        if audio_file is not None:
+            # Speichern der Audiodatei temporär auf dem Server
+            with wave.open("uploaded_audio.wav", "wb") as audio_f:
+                audio_f.setnchannels(settings.NUM_CHANNELS)
+                audio_f.setsampwidth(settings.BIT_DEPTH // 8)
+                audio_f.setframerate(settings.SAMPLE_RATE)
+                audio_f.setcomptype(settings.COMPRESSION_TYPE, 'NONE')
+                audio_f.writeframes(audio)
+            # Aufruf der read_in-Funktion mit dem Dateipfad
+            hashes = read_in("uploaded_audio.wav")
+        
+            # Löschen der temporären Audiodatei
+            os.remove("uploaded_audio.wav")
+        
+            return hashes
+        else:
+            return None
+    
 
 # Uploading audio file
 audio_file = st.file_uploader("Please upload an audio file.", type=[".mp3", ".wav", ".ogg"])
+
+
+# Instanz der Multimedia-Datenbank erstellen
+db = MultimediaDatabase()
 
 # Button to start the recognition
 if st.button("Start recognition"):
     if audio_file is not None:
         st.write("Recognition started...")
         # Calculate hashes of the uploaded audio
-        hashes = calculate_hashes(audio_file)
+        song_import = SongImport()
+        hashes = song_import.calculate_hashes(audio_file)
         if hashes:
             # Search for matching hashes in the database
-            matching_id = find_matching_hashes(hashes)
+            matching_id = db.find_matching_hashes(hashes)
             if matching_id:
-                st.write(f"Matching ID found in the database: {matching_id}")
+                # Fetch title and image path from the database using the matching ID
+                title, image_path = db.get_title_and_image_by_id(matching_id)
+                if title and image_path:
+                    st.write(f"Matching ID found in the database: {matching_id}")
+                    st.write(f"Title: {title}")
+                    st.image(image_path, caption='Album Cover', use_column_width=True)
+                else:
+                    st.write("Error retrieving title and image path from the database.")
             else:
                 st.write("No matching entry found in the database.")
             st.write("Recognition finished!")
--- pages/music_import.py
+++ pages/music_import.py
@@ -1,141 +1,88 @@
 import streamlit as st
 from tinydb import TinyDB, Query
-from typing import Optional, BinaryIO
-import uuid
 import os
-from music_analyzer import read_in
+import uuid
+from music_analyzer import read_in  
 import wave
-import settings
+import settings  
 
-# Page header configuration:
-st.set_page_config(page_title="Music and Image Import", page_icon="🎵", layout="wide")
+# Klasse für die Multimedia-Datenbank
+class MultimediaDatabase:
+    def __init__(self, db_path: str = './db/multimedia_database.json') -> None:
+        self.db = TinyDB(db_path)
 
-# Markdown title for the app
-st.title("Music import tool for adding new songs to the database")
-st.write(" ")
-st.write(" ")
-st.write(" ")
+    def insert_entry(self, entry_data: dict) -> None:
+        """Füge einen Eintrag in die Datenbank ein."""
+        self.db.insert(entry_data)
 
-# Connect to the database
-db = TinyDB('./db/multimedia_database.json')
+    def get_entry_by_id(self, entry_id: str) -> dict:
+        """Hole einen Eintrag aus der Datenbank anhand der ID."""
+        Entry = Query()
+        return self.db.get(Entry.id == entry_id)
 
-def upload_files(audio_file: Optional[BinaryIO], image_file: Optional[BinaryIO], title: str, interpret: str, album: str) -> None:
-    if audio_file is not None and image_file is not None:
-        audio = audio_file.read()
-        image = image_file.read()
-        # Generating a unique ID for the entry
-        entry_id = str(uuid.uuid4())
-        # Create a directory with title and ID
-        directory_audio = "Audio"
-        os.makedirs(directory_audio, exist_ok=True)
-        # Create a directory with Cover and ID
-        directory_image = "Image"
-        os.makedirs(directory_image, exist_ok=True)
-        # Save audio file
-        audio_filename = os.path.join(directory_audio, f"{title}_{entry_id}.wav")
-        with wave.open(audio_filename, 'w') as audio_f:
-            audio_f.setnchannels(settings.NUM_CHANNELS)
-            audio_f.setsampwidth(settings.BIT_DEPTH // 8)
-            audio_f.setframerate(settings.SAMPLE_RATE)
-            audio_f.setcomptype(settings.COMPRESSION_TYPE, 'NONE')
-            audio_f.writeframes(audio)
-            
-    
-            # audio_f.write(audio)
-        # Save image file
-        image_filename = os.path.join(directory_image, f"{title}_{entry_id}.png")
-        with open(image_filename, 'wb') as image_f:
-            image_f.write(image)
-        
-        hashes = read_in(audio_filename)
+# Klasse für den Song-Import
+class SongImporter:
+    def __init__(self, database: MultimediaDatabase) -> None:
+        self.database = database
 
-        # Inserting audio file and image file data into the database
-        db.insert({'id': entry_id, 'type': 'multimedia', 'title': title, 'artist': interpret, 'album': album, 'audio_file_path': audio_filename, 'image_file_path': image_filename, 'hashes' : hashes})
-        
-def get_info_by_id(entry_id: str) -> Optional[str]:
-    """
-    Function to retrieve the image file path by entry ID from the database.
+    def upload_files(self, audio_file, image_file, title: str, interpret: str, album: str) -> None:
+        """Lade Audio-, Bilddateien und hash hoch und füge sie in die Datenbank ein."""
+        if audio_file is not None and image_file is not None:
+            audio = audio_file.read()
+            image = image_file.read()
+            entry_id = str(uuid.uuid4())
+            directory_audio = "Audio"
+            os.makedirs(directory_audio, exist_ok=True)
+            directory_image = "Image"
+            os.makedirs(directory_image, exist_ok=True)
+            audio_filename = os.path.join(directory_audio, f"{title}_{entry_id}.wav")
+            with wave.open(audio_filename, 'w') as audio_f:
+                audio_f.setnchannels(settings.NUM_CHANNELS)
+                audio_f.setsampwidth(settings.BIT_DEPTH // 8)
+                audio_f.setframerate(settings.SAMPLE_RATE)
+                audio_f.setcomptype(settings.COMPRESSION_TYPE, 'NONE')
+                audio_f.writeframes(audio)
+            image_filename = os.path.join(directory_image, f"{title}_{entry_id}.png")
+            with open(image_filename, 'wb') as image_f:
+                image_f.write(image)
+            hashes = read_in(audio_filename)
+            self.database.insert_entry({'id': entry_id, 'type': 'multimedia', 'title': title, 'artist': interpret, 'album': album, 'audio_file_path': audio_filename, 'image_file_path': image_filename, 'hashes' : hashes})
 
-    Parameters:
-        entry_id (str): ID of the entry to retrieve.
+# Klasse für die Main
+class Main:
+    def __init__(self) -> None:
+        self.db = MultimediaDatabase()
+        self.song_importer = SongImporter(self.db)
 
-    Returns:
-        str or None: File path of the image, or None if entry not found.
-    """
-    Entry = Query()
-    result = db.get(Entry.id == entry_id)
-    if result:
-        image_file_path = result.get('image_file_path')
-        return image_file_path
-    else:
-        return None
-    
-def get_title_by_id(entry_id: str) -> Optional[str]:
-    
-    Entry = Query()
-    result = db.get(Entry.id == entry_id)
-    if result:
-        title = result.get('title')
-        return title
-    else:
-        return None
+    def run(self) -> None:
+        """Starte die Anwendung."""
+        st.set_page_config(page_title="Music and Image Import", page_icon="🎵", layout="wide")
+        st.title("Music import tool for adding new songs to the database")
 
-def main() -> None:
+        # Abschnitt zum Hochladen von Dateien und Metadaten
+        st.subheader("Import a new song by uploading an audio and an image file. Choose a title, artist, and album for the song.")
+        audio_file = st.file_uploader("Please upload an audio file.", type=[".wav"])
+        image_file = st.file_uploader("Please upload an image file.", type=[".png"])
+        title = st.text_input("Enter the title of the song:")
+        interpret = st.text_input("Enter the artist of the song:")
+        album = st.text_input("Enter the albumname of the song:")
+        if st.button("Start import"):
+            st.write("Import started...")
+            self.song_importer.upload_files(audio_file, image_file, title, interpret, album)
+            st.write("Import finished!")
 
-    st.subheader("Import a new song by uploading an audio and an image file. Choose a title, artist, and album for the song.")
-    # Uploading audio file
-    st.write(" ")
+        # Abschnitt zum Abrufen von Informationen über einen Song
+        st.subheader("Fetch information about a song by entering the ID of the entry.")
+        entry_id = st.text_input("Enter the ID of the entry:")
+        if st.button("Fetch Information"):
+            entry = self.db.get_entry_by_id(entry_id)
+            if entry:
+                st.write(f"Title: {entry['title']}")
+                st.image(entry['image_file_path'], caption='Album Cover', use_column_width=True)
+            else:
+                st.write("Entry not found!")
 
-    audio_file = st.file_uploader("Please upload an audio file.", type=[".wav"])
-    # Uploading image file
-    image_file = st.file_uploader("Please upload an image file.", type=[".png"])
-
-
-    # Text inputs for title, artist, and album
-    title = st.text_input("Enter the title of the song:")
-    interpret = st.text_input("Enter the artist of the song:")
-    album = st.text_input("Enter the albumname of the song:")
-
-    # Start import button
-    if st.button("Start import"):
-        st.write("Import started...")
-        # Importing the audio and image files into the database
-        upload_files(audio_file, image_file, title, interpret, album)
-
-        st.write("Import finished!")
-
-    st.write(" ")
-    st.write(" ")
-    st.write(" ")
-
-
-    st.subheader("Fetch information about a song by entering the ID of the entry.")
-    st.write(" ")
-
-    # Text input for entry ID
-    entry_id = st.text_input("Enter the ID of the entry:")
-
-    if st.button("Fetch Information"):
-        # Get image file path from the database
-        image_file_path = get_info_by_id(entry_id)
-        if image_file_path:
-            # Get the title from the database
-            title = get_title_by_id(entry_id)
-            st.write(f"Title: {title}")
-            st.image(image_file_path, caption='Album Cover', use_column_width=True)
-        else:
-            st.write("Entry not found!")
-
+# Main
 if __name__ == "__main__":
-    main()
-
-
-
-
-
-
-
-
-
-
-
+    main = Main()
+    main.run()

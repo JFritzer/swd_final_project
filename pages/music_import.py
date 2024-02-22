749a0edfd43f1e23f33aaaf0f22d3a726a573ff9
YeetMiche <64234954+YeetMiche@users.noreply.github.com>
2024-02-22 18:16:45 +0100

    changed file types for import image

--- pages/music_import.py
+++ pages/music_import.py
@@ -17,7 +17,7 @@ class Main:
         # Abschnitt zum Hochladen von Dateien und Metadaten
         st.subheader("Import a new song by uploading an audio and an image file. Choose a title, artist, and album for the song.")
         audio_file = st.file_uploader("Please upload an audio file.", type=[".wav"])
-        image_file = st.file_uploader("Please upload an image file.", type=[".png"])
+        image_file = st.file_uploader("Please upload an image file.", type=[".png", ".jpg", ".jpeg"])
         title = st.text_input("Enter the title of the song:")
         interpret = st.text_input("Enter the artist of the song:")
         album = st.text_input("Enter the albumname of the song:")
